81f4d879fbf083136c6500525bcac7bc120f99da
Aravind-22-N <aravindrohit45@gmail.com>
2024-06-10 10:12:30 +0530

    Create Simple_ps.c

---
+++ Simple_ps.c
@@ -0,0 +1,7 @@
+echo "# Simple_ps.c" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Aravind-22-N/Simple_ps.c.git
+git push -u origin main
